c09b6a88b6295b92fd90165141b6b55096aefca5
blake-farman <blake.farman@gmail.com>
2026-09-01 16:35:29 -0500

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,104 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "frontmatter",
+  "level": "1",
+  "url": "frontmatter.html",
+  "type": "Front Matter",
+  "number": "",
+  "title": "Front Matter",
+  "body": "  Blake Farman Mathematics and Statistics Louisiana Tech University        copyright      "
+},
+{
+  "id": "contact_information",
+  "level": "1",
+  "url": "contact_information.html",
+  "type": "Section",
+  "number": "1",
+  "title": "Contact Information",
+  "body": " Contact Information      Professor  Dr. Blake Farman    Phone Number  318.257.2951    Email Address   bfarman@latech.edu    Website     Office  Nethken 235       Office Hours     Mon\/Wed\/Fri  1:00 PM  2:00 PM    Please make an appointment using my booking page .    Preferred Method of Communication  The best way to communicate with me during the quarter is through email. I monitor my email during regular business hours and try to respond within one business day.    Official University Email Addresses  The University provides each student with an email address and all official course correspondence will only be conducted using official university email addresses.    Email Etiquette  When you send an email, your message should contain the following information   Subject  A few words that describe the content of your email along with the course identifier (Math 4083\/5083 001)    Salutation  A professional greeting such as Dear or Hello followed by the appropriate honorific and the recipient's last name (  Dr. X).   If the person you are addressing holds a doctorate ( a PhD ), then you should address the recipient as Dr. X, never Mr. X, Ms. X, or Mrs. X.    If you are unsure, Professor X is generally a safe way to address a faculty member.       Body  A concise message that clearly expresses the purpose of your email.    Signature  A simple closing (e.g. Sincerely,  Thank you,  Best regards,  ) followed by your first and last name.   For your convenience, included below is an example email.   Sample Email  Subject: Meeting - Math 4083\/5083 001  Dear Dr. Farman,  I am having trouble with this week's homework. I have tried working through the examples in the text, but I am struggling to understand the concepts involved. Could we schedule a time to meet before the next class to discuss some of the concepts in more detail?  Thank you,  Jane Doe    "
+},
+{
+  "id": "course_information",
+  "level": "1",
+  "url": "course_information.html",
+  "type": "Section",
+  "number": "2",
+  "title": "Course Information",
+  "body": " Course Information   Course Description  This course will utilize the logical foundation and proof techniques developed in pre-requisite courses to explore the basic constructions in modern algebra: rings, modules, and groups.    Course Prerequisites  A grade of C or better in MATH 3073 (Previously MATH 307) or MATH 3113 (Previously MATH 311) is required to enroll in this course.  In particular, students are expected to have experience with the following topics.   propositional logic, such as  conjunction,  disjunction  negation,  conditionals, biconditionals and logical equivalence,  contraposition,      first-order logic, such as  predicates,  universal quantification,  existential quantification      basic set theory, such as  (indexed) union,  (indexed) intersection,  complement,   functions  domain and codomain,  composition,  injectivity,  surjectivity,  bijectivity     equivalence relations and classes      basic proof techniques, such as  direct proof,  proof by contraposition,  proof by contradiction,  proof by induction,         Student Learning Outcomes  The primary goal of this course is to gain familiarity with basic algebraic structures (rings, modules, and groups) and how they interact with one another (actions, homomorphisms, ). An equally important goal of this course is to improve students’ ability to read and write mathematical proofs.    Course Topics  This course can be divided into three parts.   Rings  We first explore the basic properties and arithmetic of the integers, including modular arithmetic. We generalize these properties to other sets equipped with operations that behave like addition and multiplication, then study structure preserving functions and constructions with rings. Depending on time and interest, we may also explore some classes of nice rings ( integral domains, unique factorization domains, principal ideal domains, polynomial rings).    Modules  We generalize the familiar notion of a vector space from linear algebra by allowing our scalars to come from a ring rather than a field. We study the analogous structure preserving functions and constructions for these objects.    Groups  We consider the structures that arise from a set equipped with a single operation. As with rings and modules, we study the analogous structure preserving functions and constructions.       Instructional Methods  This course is offered as a face-to-face course. Learning will be facilitated through traditional lecture, group work\/activities, homework, and in-class assessments.   "
+},
+{
+  "id": "course_topics-2",
+  "level": "2",
+  "url": "course_information.html#course_topics-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "vector space "
+},
+{
+  "id": "evaluation",
+  "level": "1",
+  "url": "evaluation.html",
+  "type": "Section",
+  "number": "3",
+  "title": "Evaluation",
+  "body": " Evaluation   Exams  There will be one in-class midterm exam and a cumulative final exam. The exams are scheduled as follows:     Exam  Date    Midterm Exam  Wednesday, October 14, 2026    Final Exam  Wednesday, November 18, 2026         The instructor reserves the right to modify the schedule of in class exams.    The final exam date and time are scheduled in advance. Failure to appear at the final exam will result in a grade of zero for the exam.        Participation and Engagement  Active participation is essential for success in this course. Students are expected to attend all classes, contribute to discussions, and engage in collaborative activities. This includes asking questions, sharing insights, and working effectively during in-class exercises or group work.  Constructive engagement allows students to deepen their understanding of the material, learn from their peers, and build essential problem-solving and communication skills that will be valuable in future coursework and careers.    Homework  Homework assignments are a critical component of the course. They are designed to reinforce the concepts covered in lectures and to give you practice writing careful mathematical arguments. Homework is neither collected nor graded directly; instead, the quizzes are drawn from the assigned problems. Collaboration is encouraged, but you should be able to reproduce every solution on your own.    Quizzes  Quizzes will be administered periodically to assess understanding of key concepts and ensure students are keeping up with the material. These will be short, focused assessments designed to reinforce learning and identify areas for improvement. They are an opportunity to practice applying course concepts and receive feedback on your progress.    Weights  Grades will be calculated using the weights below.   Evaluation Criteria    Grade Category  Weight    Quizzes  50%    Midterm Exam  25%    Final Exam  25%       Grading Scale  Letter grades will be assigned based on the scale below.   Grading Scale    Average  Letter Grade     A     B     C     D     F      "
+},
+{
+  "id": "exams-4",
+  "level": "2",
+  "url": "evaluation.html#exams-4",
+  "type": "Warning",
+  "number": "3.1",
+  "title": "",
+  "body": "    The instructor reserves the right to modify the schedule of in class exams.    The final exam date and time are scheduled in advance. Failure to appear at the final exam will result in a grade of zero for the exam.     "
+},
+{
+  "id": "policies",
+  "level": "1",
+  "url": "policies.html",
+  "type": "Section",
+  "number": "4",
+  "title": "Course Policies and Procedures",
+  "body": " Course Policies and Procedures   At a minimum, all policies stated in the current Louisiana Tech University Student Handbook should be followed. Additional class policies follow.    Textbook   Required Textbook   Algebra: Notes from the Underground , First edition, by Paolo Aluffi, ISBN 978-1-108-95823-3.     Attendance Policy  Per University Policy 2206 :    All students are expected to attend classes in which they are enrolled regularly and punctually. Failure to do so may jeopardize a student's scholastic standing and may lead to suspension from the College or University.  The student is responsible for making arrangements satisfactory to the instructor regarding absences including prior notification of planned absences, providing documentation if needed, and making up of examinations and work as required. For other emergency absences, the student shall submit excuses for class absences to the appropriate instructor within three class days following the student's return to their respective class.    As noted above, students are obligated to complete all assigned work promptly, to attend class regularly, and to participate in whatever class discussion may occur. Any behavior that interferes with the ability of other students to learn is unacceptable. If any of these become an issue, you will be asked to leave class for the day.  Your phone should be on silent and put away during class. Headphones should also be put away during class. Texting and browsing social media during class is rude and disrespectful. Please refrain from using your phone during class unless explicitly stated otherwise. If you are caught with a cell phone on your desk, in your lap, or anywhere in view while taking a quiz or exam, it will be treated as a case of academic dishonesty. Smart watches are also prohibited during exams; please put them away during those times.    Late Policy  Late work will not be accepted. Students who cannot submit work due to a University-related activity requiring an absence from class must arrange for work to be submitted before leaving.    Make-up Policy  In the event of one missed exam due to absence, the Final Exam can be used to replace the missing exam. Further missed exams will result in a grade of zero. If a University sponsored activity requires you to miss an exam, you must complete the exam in advance.         Academic Honor Code  In accordance with the Academic Honor Code , students pledge the following:    Being a student of higher standards, I pledge to embody the principles of academic integrity.    Any suspected case of academic dishonesty will be reported. If it is determined that academic misconduct has occurred, the student will receive a grade of F for the course and may be dismissed from the University.   "
+},
+{
+  "id": "student_services",
+  "level": "1",
+  "url": "student_services.html",
+  "type": "Section",
+  "number": "5",
+  "title": "Student Services",
+  "body": " Student Services   Accommodations for Students with Disabilities  Students needing testing or classroom accommodations based on a disability must be registered with the Louisiana Tech University Office of Testing & Disability Services. Students are encouraged to discuss those needs with the instructor as soon as possible. If you do not have a current accommodation memo, visit the Office of Testing & Disability Services for assistance.    Counseling Services  Students have access to confidential counseling services at no additional cost. Visit the Louisiana Tech Counseling Services website to learn more.    Non-Discrimination  Louisiana Tech University is committed to the principle of providing the opportunity for learning and development of all qualified citizens without regard to race, color, national origin, religion, age, sex, sexual orientation, marital status, or disability. Persons with concerns may contact the Louisiana Tech Director of Title IX Compliance:        Mortissa D. Harvey    Phone  318.257.5911    Office  Wyly Tower 1536    Email  mharvey@latech.edu      Section 504 is a federal law designed to protect the rights of individuals with disabilities in programs and activities that receive Federal financial assistance from the U.S. Department of Education. Persons with concerns may contact the Section 504 Coordinator:        Annie Jantz    Phone  318.257.2445    Office  Division of Student Affairs; Keeny Hall 305    Email  ajantz@latech.edu        Emergency Notification System ( ENS )  All Louisiana Tech students are strongly encouraged to enroll and update their contact information in the Emergency Notification System. It takes just a few seconds to ensure you’re able to receive important text and voice alerts in the event of a campus emergency.    Drop with a W date  The latest date a student may drop a course (or resign his\/her entire schedule) with a W grade for Fall 2026 is Friday, October 30, 2026.    Canvas and Workday Help  Direct questions\/problems to the Computing Center Help Desk ( helpdesk@latech.edu or 318-257-5300).   "
+},
+{
+  "id": "schedule",
+  "level": "1",
+  "url": "schedule.html",
+  "type": "Section",
+  "number": "6",
+  "title": "Schedule",
+  "body": " Schedule   September 7 September 13     September 11  1.1 The Well-Ordering Principle and Induction  Homework: Read 1.2, 1.3; Exercises 1.2, 1.3       September 14 September 20     September 14  1.2 Division with Remainder in  1.3 Greatest Common Divisors  Homework: Read 1.4; Exercises 1.4, 1.8, 1.10    September 16  1.4 The Fundamental Theorem of Arithmetic  Homework: Read 2.1, 2.2 (and Appendix A.8 Relations; Equivalence Relations and Quotient Sets, if necessary); Exercises 1.17, 1.21, 1.23, 1.25    September 18  2.1 Equivalence Relations and Quotients  2.2 Congruence mod  Homework: Read 2.3, 2.4; Exercises 2.1, 2.2, 2.3       September 21 September 27     September 21  2.3 Algebra in  2.4 Properties of the Operations and  Homework: Read 3.1, 3.2; Exercises 2.4, 2.9, 2.10, 2.11    September 23  3.1 Definition and Examples  3.2 Basic Properties  Homework: Read 3.3; Exercises 3.5, 3.6, 3.7, 3.8    September 25  3.3 Special Types of Rings  Homework: Read 4.1, 4.2; Exercises 3.12, 3.13, 3.14, 3.17, 3.20       September 28 October 4     September 28  4.1 Cartesian Products  4.2 Subrings  Homework: Read 4.3, 4.4; Exercises 4.2, 4.3, 4.4, 4.5    September 30  4.3 Ring Homomorphisms  4.4 Isomorphisms of Rings  Homework: Read 5.1, 5.2; Exercises 4.13, 4.14, 4.19    October 02  5.1 Canonical Decomposition, I  5.2 Kernels and Ideals  Homework: Read 5.3; Exercises 5.4, 5.5, 5.7, 5.9, 5.10, 5.12, 5.13, 5.14, 5.15, 5.16, 5.18       October 5 October 11     October 05  5.3 Quotient Rings  Homework: Read 5.4, 5.5; Exercise 5.20    October 07  5.4 Canonical Decomposition, II  5.5 The First Isomorphism Theorem  Homework: Read 5.6, 5.7; Exercises 5.8, 5.23, 5.25, 5.26    October 09  5.6 The Chinese Remainder Theorem  5.7 The Third Isomorphism Theorem  Homework: Exercises 5.33, 5.34, 5.36       October 12 October 18     October 12  Review for the Midterm Exam    October 14   Midterm Exam (Chapters 1 5).  Homework: Read 8.1    October 16  8.1 Vector Spaces and Ideals, Revisited  Homework: Read 8.2; Exercises 8.1, 8.2, 8.3       October 19 October 25     October 19  8.2 The Category of -Modules  Homework: Read 8.3; Exercises 8.6, 8.10, 8.12    October 21  8.3 Submodules, Direct Sums  Homework: Read 8.4; Exercises 8.16, 8.17, 8.19    October 23  8.4 Canonical Decomposition and Quotients  Homework: Read 8.5; Exercises 8.15, 8.23, and prove Claim 8.38       October 26 November 1     October 26  8.5 Isomorphism Theorems  Homework: Read 10.1; Exercise 8.22    October 28  10.1 The Category of Abelian Groups  Homework: Read 10.2; Exercises TBA    October 30  10.2 Cyclic Groups, and Orders of Elements  Homework: Read 11.1; Exercises TBA       November 2 November 8     November 02  11.1 Groups and their Category  Homework: Read 11.2; Exercises 11.1, 11.2, 11.3, 11.4, 11.7    November 04  11.2 Why Groups? Actions of a Group  Homework: Read 11.3; Exercise 11.14    November 06  11.3 Cyclic, Dihedral, Symmetric, Free Groups  Homework: Read 11.4; Exercises 11.28, 11.30, 11.33       November 9 November 15     November 09  11.4 Canonical Decomposition, Normality, and Quotients  Homework: Read 11.5; Exercises 11.34, 11.36, 11.39    November 11  11.5 Isomorphism Theorems  Homework: Exercise 11.40    November 13  TBA       November 16 November 22     November 16  Review for the Final Exam    November 18   Final Exam (cumulative).        The instructor reserves the right to modify the schedule as needed.    "
+},
+{
+  "id": "schedule-13-1",
+  "level": "2",
+  "url": "schedule.html#schedule-13-1",
+  "type": "Warning",
+  "number": "6.1",
+  "title": "",
+  "body": " The instructor reserves the right to modify the schedule as needed.  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
